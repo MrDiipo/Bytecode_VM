af6486d6326cccd1c9aaa2287dd34b41122f905b
oladipupo <elegbedeola@gmail.com>
2022-09-10 04:28:29 +0100

    initial commit

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
-#include <stdio.h>
+//#include "common.h"
+//
+//#ifndef clox_common_h
+#define clox_common_h
 
-int main() {
+#include <stdio.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+int main(int argc, const char* argv[]) {
     printf("Hello, World!\n");
     return 0;
 }
