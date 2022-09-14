1564af3fdcbf7a69d94fd8dc296d990113dc2bcc
oladipupo <elegbedeola@gmail.com>
2022-09-15 00:20:58 +0100

    implement bytecode sequence

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
+#include <stdlib.h>
 #include "common.h"
 
 
 int main(int argc, const char* argv[]) {
     printf("Hello, World!\n");
+    for (int i = 1; i <= 10; ++i) {
+        printf("%d ", 1 + (rand() % 6)); // display random die value
+    }
     return 0;
 }
