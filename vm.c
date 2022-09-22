a1ad9d22cc35ea5753c6e4a6545c4a0f1ebe030a
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2022-09-22 14:13:58 +0100

    implement stack memory operations

--- vm.c
+++ vm.c
@@ -40,17 +40,26 @@ static InterpretResult run() {
 
     for( ;; ){
 #ifdef DEBUG_TRACE_EXECUTION
+        printf("       ");
+        for(Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+            printf("[  ");
+            printValue(*slot);
+            printf("   ]");
+        }
+        printf("\n");
         disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
 #endif
         uint8_t instruction;
         switch (instruction = READ_BYTE()) {
             case OP_CONSTANT: {
                 Value constant = READ_CONSTANT();
+                push(constant);
                 printValue(constant);
                 printf("\n");
                 break;
             }
             case OP_RETURN: {
+                printValue(pop());
                 return INTERPRET_OK;
             }
         }
