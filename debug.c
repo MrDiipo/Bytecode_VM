641892275054daadbd58b434f942833849a936be
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2022-09-22 16:07:48 +0100

    implement unary operations

--- debug.c
+++ debug.c
@@ -42,6 +42,8 @@ int disassembleInstruction(Chunk* chunk, int offset){
             return constantInstruction("OP_CONSTANT", chunk, offset);
         case OP_RETURN:
             return simpleInstruction("OP_RETURN", offset);
+        case OP_NEGATE:
+            return simpleInstruction("OP_NEGATE", offset);
         default:
             printf("Unknown opcode %d\n", instruction);
             return offset + 1;
--- vm.c
+++ vm.c
@@ -58,6 +58,9 @@ static InterpretResult run() {
                 printf("\n");
                 break;
             }
+            case OP_NEGATE:
+                push(-pop());
+                break;
             case OP_RETURN: {
                 printValue(pop());
                 return INTERPRET_OK;
