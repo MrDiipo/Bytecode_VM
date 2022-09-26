0bea6474bdca96effe8d568edff372a96e0ca90e
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2022-09-26 09:18:25 +0100

    implement scanner phase

--- compiler.c
+++ compiler.c
@@ -5,4 +5,15 @@
 
 void compile(const char* source) {
     initScanner(source);
+    for ( ; ; ) {
+        Token token = scanTokent();
+        if (token.line != line) {
+            printf("%4d ", token.line);
+            line = token.line;
+        } else {
+            printf("     |  ");
+        }
+        printf("%2d  '%.*s'\n", token.type, token.length, tken.start);
+        if(token.type == TOKEN_EOF) break;
+    }
 }
