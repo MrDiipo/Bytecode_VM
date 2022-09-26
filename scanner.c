4e57b5b24baddf4376c0f01ea1a6b5eaab663981
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2022-09-26 09:38:26 +0100

    implement lexical grammar

--- scanner.c
+++ scanner.c
@@ -22,6 +22,19 @@ static bool isAtEnd(){
     return *scanner.current == '\0';
 }
 
+static char advance() {
+    scanner.current++;
+    return scanner.current[-1];
+}
+
+static bool match(char expected) {
+    if (isAtEnd()) return false;
+    if (*scanner.current != expected) return false;
+
+    scanner.current++;
+    return true;
+}
+
 static Token makeToken(TokenType type) {
     Token  token;
     token.type = type;
@@ -45,5 +58,25 @@ static Token errorToken(const char* message) {
 Token scanToken() {
     scanner.start = scanner.current;
     if (isAtEnd()) return makeToken(TOKEN_EOF);
+
+    char c = advance();
+
+    switch(c) {
+        case '(': return makeToken(TOKEN_LEFT_PAREN);
+        case ')': return makeToken(TOKEN_RIGHT_PAREN);
+        case '{': return makeToken(TOKEN_LEFT_BRACE);
+        case '}': return makeToken(TOKEN_RIGHT_BRACE);
+        case ';': return makeToken(TOKEN_SEMICOLON);
+        case ',': return makeToken(TOKEN_COMMA);
+        case '.': return makeToken(TOKEN_DOT);
+        case '-': return makeToken(TOKEN_MINUS);
+        case '+': return makeToken(TOKEN_PLUS);
+        case '/': return makeToken(TOKEN_SLASH);
+        case '*': return makeToken(TOKEN_STAR);
+        case '!': return makeToken(match('=') ? TOKEN_BANG_EQUAL : TOKEN_BANG);
+        case '=': return makeToken(match('=') ? TOKEN_EQUAL_EQUAL : TOKEN_EQUAL);
+        case '<': return makeToken(match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
+        case '>': return makeToken(match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+    }
     return errorToken("Unexpected Character");
 }
