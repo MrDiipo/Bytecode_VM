601176757da245049adab5410f9bae4c6ca7ad1f
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2022-09-27 15:07:28 +0100

    implement syntax error reporting

--- compiler.c
+++ compiler.c
@@ -1,19 +1,63 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "compiler.h"
 #include "common.h"
 #include "scanner.h"
 
-void compile(const char* source) {
-    initScanner(source);
-    for ( ; ; ) {
-        Token token = scanTokent();
-        if (token.line != line) {
-            printf("%4d ", token.line);
-            line = token.line;
-        } else {
-            printf("     |  ");
-        }
-        printf("%2d  '%.*s'\n", token.type, token.length, tken.start);
-        if(token.type == TOKEN_EOF) break;
+typedef struct {
+    Token current;
+    Token previous;
+    bool hadError;
+    bool panicMode;
+} Parser;
+
+Parser parser;
+
+static void errorAt(Token *token, const char* message) {
+    if (parser.panicMode) return;
+    parser.panicMode = true;
+    fprintf(stderr, "[line %d} Error", token->line);
+
+    if (token->type == TOKEN_EOF) {
+        fprintf(stderr, " at end ");
+    } else if (token->type == TOKEN_ERROR) {
+        // Nothing
+    } else {
+        fprintf(stderr, " at '%.*s'", token->length, token->start);
     }
+    fprintf(stderr, ": %s\n", message);
+    parser.hadError = true;
+}
+
+static void errorAtCurrent(const char *message) {
+    errorAt(&parser.current, message);
+}
+
+static void advance(){
+    parser.previous = parser.current;
+
+    for ( ; ; ) {
+        parser.current = scanToken();
+        if (parser.current.type != TOKEN_ERROR) break;
+
+        errorAtCurrent(parser.current.start);
+    }
+}
+
+static void consume(TokenType type, const char* message) {
+    if (parser.current.type) {
+       advance();
+        return;
+    }
+    errorAtCurrent(message);
+}
+
+void compile(const char* source, Chunk* chunk) {
+    initScanner(source);
+
+    parser.hadError = false;
+    parser.panicMode = false;
+    advance();
+    expression();
+    consume(TOKEN_EOF, "Expect end of expression");
 }
--- compiler.h
+++ compiler.h
@@ -2,6 +2,8 @@
 #ifndef BYTECODE_VM_COMPILER_H
 #define BYTECODE_VM_COMPILER_H
 
-void compile(const char* source);
+#include "vm.h"
+
+void compile(const char* source, Chunk *chunk);
 
 #endif //BYTECODE_VM_COMPILER_H
