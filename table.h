934df7f6d51ad66db34911a01b6b7d73a7fc6892
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2022-10-18 15:44:32 +0100

    Add utility functions to the Hash Map

--- table.h
+++ table.h
@@ -19,4 +19,10 @@ typedef struct {
 void initTable(Table* table);
 void freeTable(Table* table);
 
+bool tableGet(Table* table, ObjString* key, Value* value);
+bool tableSet(Table* table, ObjString* key, Value value);
+
+bool tableDelete(Table* table, ObjString* key);
+void tableAddAll(Table* from, Table* to);
+
 #endif //BYTECODE_VM_TABLE_H
