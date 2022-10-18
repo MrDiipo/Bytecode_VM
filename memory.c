fe640b17dbe96b07ecaff6122c1815f8cc29e91c
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2022-10-18 10:34:50 +0100

    Add primitive Garbage Collection algorithm

--- memory.c
+++ memory.c
@@ -1,6 +1,12 @@
 
 #include <stdlib.h>
 #include "memory.h"
+#include "object.h"
+#include "vm.h"
+
+extern VM vm;
+
+void freeObject(Obj *object);
 
 void* reallocate(void* pointer, size_t oldSize, size_t newSize) {
     if (newSize == 0) {
@@ -12,4 +18,24 @@ void* reallocate(void* pointer, size_t oldSize, size_t newSize) {
         exit(1);
     }
     return result;
+}
+
+void freeObjects() {
+    Obj* object = vm.objects;
+    while (object != NULL) {
+        Obj* next = object -> next;
+        freeObject(object);
+        object = next;
+    }
+}
+
+void freeObject(Obj* object) {
+    switch (object -> type) {
+        case OBJ_STRING: {
+            ObjString* string = (ObjString*)object;
+            FREE_ARRAY(char, string->chars, string->length + 1);
+            FREE(ObjString, object);
+            break;
+        }
+    }
 }
--- memory.h
+++ memory.h
@@ -6,6 +6,8 @@
 
 #define ALLOCATE(type,count) (type*)reallocate(NULL, 0, sizeof(type) * (count))
 
+#define FREE(type, pointer) reallocate(pointer, sizeof(type), 0)
+
 #define GROW_CAPACITY(capacity) ((capacity) < 8 ? 8 : (capacity) * 2)
 
 #define FREE_ARRAY(type, pointer, oldCount) reallocate(pointer, sizeof (type) * (oldCount), 0)
